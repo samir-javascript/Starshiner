ae3d486de7835f51a513ebe8f3f7ad5d08d06845
samir-javascript <samirjavascript00@gmail.com>
2024-06-15 21:23:22 +0200

    push

--- lib/features/cartSlice.ts
+++ lib/features/cartSlice.ts
@@ -1,56 +1,4 @@
-// import { createSlice } from '@reduxjs/toolkit';
-// import { updateCart } from '../updateCart';
-// import { ProductProps } from '@/types';
 
-
-
-// const initialState = {cartItems: [], shippingAddress: {} , paymentMethod: ""} as {
-//   paymentMethod: string;
-//   cartItems: ProductProps[];
-//   shippingAddress: {}
-// }
-
-// const cartSlice = createSlice({
-//   name: 'cart',
-//   initialState,
-//   reducers: {
-//     addToCart: (state, action) => {
-//       const { ...item } = action.payload;
-//       const existItem = state.cartItems.find((x: ProductProps) => x._id === item._id);
-//       if (existItem) {
-//         state.cartItems = state.cartItems.map((x: ProductProps) =>
-//           x._id === existItem._id ? item : x
-//         );
-//       } else {
-//         state.cartItems = [...state.cartItems, item];
-//       }
-//       return updateCart(state);
-//     },
-//     removeFromCart: (state, action) => {
-//       state.cartItems = state.cartItems.filter((x: ProductProps) => x._id !== action.payload);
-      
-//       return updateCart(state);
-//     },
-    
-//     decreaseQty: (state, action) => {
-//       return updateCart(state);
-//     },
-//     increaseQty: (state, action) => {
-//       return updateCart(state);
-//     },
-//     resetCart: (state, action) => {
-//       state = initialState
-//       return state;
-//     },
-//     clearCart: (state, action) => {
-//       state.cartItems = [];
-//       return updateCart(state);
-//     }
-//   }
-// });
-
-// export const { clearCart, addToCart, removeFromCart, resetCart, increaseQty, decreaseQty } = cartSlice.actions;
-// export default cartSlice.reducer;
 
 import { createSlice } from '@reduxjs/toolkit';
 import { ProductProps } from '@/types';
@@ -81,7 +29,7 @@ const cartSlice = createSlice({
       const item = action.payload;
       const existItem = state.cartItems.find((x: ProductProps) => x._id === item._id);
       if (existItem) {
-        existItem.qty += item.qty;
+        existItem.qty += 1;
       } else {
         state.cartItems = [...state.cartItems, item];
       }

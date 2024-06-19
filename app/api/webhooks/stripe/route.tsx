4eda631fc7dd482118332284e6ad2d5f3ffc39af
samir-javascript <samirjavascript00@gmail.com>
2024-06-19 20:09:08 +0200

    jh

--- app/api/webhooks/stripe/route.tsx
+++ app/api/webhooks/stripe/route.tsx
@@ -1,37 +1,41 @@
 import ConfirmationEmail from "@/components/emails/ConfirmationEmail";
-
 import Product from "@/schemas/productModel";
 import OrderModel from "@/schemas/orderModel";
 import { connectToDb } from "@/db";
 import { NextRequest, NextResponse } from "next/server";
-import { Resend} from "resend"
-import Stripe from "stripe"
+import { Resend } from "resend";
+import Stripe from "stripe";
 import { Cart } from "@/schemas/cartModel";
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
-const resend = new Resend(process.env.RESEND_API_KEY as string)
-export const POST = async(req:NextRequest) => {
-     try {
-        const event = await stripe.webhooks.constructEvent(await req.text(),
-         req.headers.get("stripe-signature") as string, 
-         process.env.STRIPE_WEBHOOK_SECRET as string)
 
-         if(event.type === "checkout.session.completed") {
-            const charge = event.data.object; 
-            const referenceId = charge?.metadata?.referenceId;
-            console.log(referenceId, "mol chi referenceId")
-            const cart = await Cart.findOne({ referenceId });
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+const resend = new Resend(process.env.RESEND_API_KEY as string);
 
-           
-            console.log('Cart data:', cart);
-        
-         }else {
-           
-            return NextResponse.json({error: "shit it doesn't work"})
-         }
-       
-     } catch (error) {
-        console.log(error)
-        return NextResponse.json({error: `${error} error soufiane`, status: 500})
-     }
-}
+export const POST = async (req: NextRequest) => {
+  try {
+    const event = await stripe.webhooks.constructEvent(
+      await req.text(),
+      req.headers.get("stripe-signature") as string,
+      process.env.STRIPE_WEBHOOK_SECRET as string
+    );
+
+    if (event.type === "checkout.session.completed") {
+      const charge = event.data.object;
+      const referenceId = charge?.metadata?.referenceId;
+      console.log(referenceId, "mol chi referenceId");
+
+      const cart = await Cart.findOne({ referenceId });
+      console.log('Cart data:', cart);
+
+      // Perform any further processing with the cart data, e.g., save order, send email
+
+      // Respond with success status
+      return NextResponse.json({ message: "Webhook processed successfully" });
+    } else {
+      return NextResponse.json({ error: "Unexpected event type" });
+    }
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: `Error: ${error}`, status: 500 });
+  }
+};
 
